d72eeab2c43cdf869add3eddc095725d9d1d67fa
rayondemiel <maximehumeau44@gmail.com>
2024-08-27 13:55:56 +0200

    reduce kebab str

--- run.py
+++ run.py
@@ -54,7 +54,7 @@ manifest_identifiers, batches = utils.batchify_jsonfile('manifests.json', batch_
 from re import sub
 
 
-def kebab(s, max_length=255):
+def kebab(s, max_length=100):
     kebab_string = sub(r"https?-", "", '-'.join(
         sub(r"(\W+)"," ",
         sub(r"[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+",
